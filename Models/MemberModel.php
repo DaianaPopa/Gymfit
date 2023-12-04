4a727df4a4831c544bcd495a100e9b8f57cf76fa
PatJNewellSHU <c1013103@hallam.shu.ac.uk>
2023-12-04 08:38:10 -0800

    hmmm

--- Models/MemberModel.php
+++ Models/MemberModel.php
@@ -1,21 +1,18 @@
 <?php
 
-include("Models/Config.php");
+include("/Models/Config.php");
 $conn = Config :: Db_Connect();
 
 class Gym_Member{
     public static function Create_Member($email,$password){
         //THis function creates a new member call it with Gym_Member :: Create_Member
-        $sql = "INSERT INTO MEMBER(Email, Member_Password)
-        $conn = Config :: Db_Connect();
-        Values($email, $password)";
+        $sql = "INSERT INTO MEMBER(Email, Member_Password)Values($email, $password)";
         $conn->query($sql);
     }
 
     public static function Get_Password_By_Email($email){
         // Get a member by email
         $sql = "SELECT Member_Password from Gym_Member Where Email = '$email'";
-        $conn = Config :: Db_Connect();
         $result = $conn->query($sql);
         if($result->num_rows < 0){
             return "none";
@@ -26,7 +23,6 @@ class Gym_Member{
 
     public static function Check_Member_Exists_By_Email($Email){
         $sql = "SELECT Member_Password From Gym_Member Where Email = '$email'";
-        $conn = Config :: Db_Connect();
         $result = $conn->query($sql);
         
         if($result->num_rows < 0){
@@ -39,7 +35,7 @@ class Gym_Member{
 
     public static function Get_Member_ID_By_Email_Passsword($email,$password){
         $sql = "SELECT Member_ID from Gym_Member Where Email = '$email' AND Where Member_Password = '$password'";
-        $conn = Config :: Db_Connect();
+        
         $result = $conn->query($sql);
         return $UserID["Member_ID"];
     }
