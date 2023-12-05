654231903c49944b8e29b42681c48c93b80c5e10
GitHoint <ozzyjosh03@gmail.com>
2023-12-05 12:13:33 +0000

    Updated style for macro pages

--- Views/macroLogger.php
+++ Views/macroLogger.php
@@ -3,16 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/main.css">
     <title>Gymfit :: Macros</title>
 </head>
 <body>
-    <h1>Track a macro</h1>
+    <div id="back-image"></div>
+    <h1 class="title">Track a macro</h1>
+    <div class="sep-bar"></div>
     date
     p amount
     c amount
     f amount
     calories 
-    
-    
 </body>
 </html>
