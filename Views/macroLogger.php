1d0839190137715050bed236e6c80754c54c73b6
DaianaPopa <116299749+DaianaPopa@users.noreply.github.com>
2023-12-05 21:49:16 +0000

    Update macroLogger.php

--- Views/macroLogger.php
+++ Views/macroLogger.php
@@ -5,6 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/main.css">
     <title>Gymfit :: Macros</title>
+    <link
+      rel="stylesheet"
+      href="../CSS/homepage.css"
+    />
 </head>
 <body>
     <div id="back-image"></div>
