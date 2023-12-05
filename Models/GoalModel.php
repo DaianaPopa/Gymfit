6ca80ca2c5a14270eefea0dbc9924b8719666ad6
PatJNewellSHU <c1013103@hallam.shu.ac.uk>
2023-12-05 04:19:34 -0800

    gaolmodel

--- Models/GoalModel.php
+++ Models/GoalModel.php
@@ -3,10 +3,10 @@ include('config.php');
 class Macro_Goals{
     public static function Create_Goal($ID,$protein,$carbs,$fat,$calories){
         $conn = Config :: Db_Connect();
-        $sql = "INSERT INTO macro_goal(Member_ID,Protein,Carbs,Fat,Calories) values($ID,)";
+        $sql = "INSERT INTO macro_goal(Member_ID,Protein,Carbs,Fat,Calories) values($ID,$protein,$carbs,$fat,$calories)";
     }
     public static function Get_Goal($ID){
-
+        
     }
 }
 ?>
