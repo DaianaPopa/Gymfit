a94d1a9c001f563d70fbc61e78f22e899f637651
PatJNewellSHU <c1013103@hallam.shu.ac.uk>
2023-12-06 01:10:55 -0800

    by bye

--- Models/Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $DBHostname = "gymfit.mysql.database.azure.com";
-    $DBUsername = "Group9";
-    $DBPassword = "Gr9!gotime";
-    $DBName = "gymfit";
-    $conn = new mysqli($DBHostname,$DBUsername,$DBPassword,$DBName);
-?>
