29bf856c6469b968b83c646ab75f5c4872b80965
Olivert32 <c2077500@hallam.shu.ac.uk>
2023-12-06 21:49:33 +0000

    Added Homepage and Trainer page

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<div class="mobile-container">
+   
+    
+      <div class="topnav">
+        <img
+        src="images/GymLogo.jpg"
+        width="50"
+        height="50"
+      />
+        <!-- Navigation links (hidden by default) -->
+        <div id="myLinks">
+          <a href="#about">About Us</a>
+          <a href="#macro">Macro Tracker</a>
+          <a href="ourTrainers.html">Our Trainers</a>
+          <a href="#workout">Work Out Planner</a>
+        </div>
+        <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+          <i class="fa fa-bars"></i>
+        </a>
+      </div>
+  </div>
