3ad0f0e47581acb1cdfd4c10a8a408f716e6c571
DaianaPopa <116299749+DaianaPopa@users.noreply.github.com>
2023-12-09 20:06:47 +0000

    Update workout.php
    looking better :3

--- Views/workout.php
+++ Views/workout.php
@@ -11,19 +11,33 @@
       href="../CSS/login.css"
       href="../CSS/main.css"
     />
+     <link
+      rel="stylesheet"
+      href="../CSS/home.css"
+    />
 
-  </head>
+ </head>
   <body>
-  <div id="back-image"></div>
+  <?php
+include("../includes/header.php");
+?>
+  <body background = "../Docs/cover.jpg" 
+style= " background-size: cover">
+    
     <h1 class="title">Workout</h1>
-    <h5><p>Getting into a consistent fitness routine can feel daunting, but it is the only way to see real results. 
-          Exercising regularly can feel hard when you are not used to it, but our thainers and classes are here to help you. 
+    <h5><p>Getting into a consistent fitness routine can feel daunting,
+    but it is the only way to see real results. 
+     <br>Exercising regularly can feel hard when you are not used to it,
+      but our thainers and classes are here to help you.    </p>
+          <a href="../Views/ourTrainers.php">See our Trainers</a>
         </p></h5>
+        <div>  
+</div>
 
     <section>
       <div class="projects grid">
       <div class="container">
-           <h5><span class="p-hover">Our Classes</span></h5>
+         
       <div class="row" style=" margin-bottom: 30px;">
       <div class="col-md-4 projects-1">
             <img src="../Docs/aerobic.png" class="img-fluid" alt="workou1">
@@ -62,36 +76,9 @@
        </div>
 </div>
 </section>
-<section>
-<div class="projects grid">
-<div class="container">
-<h5><span class="p-hover">Our Trainers</span></h5>
-<div class="row" style=" margin-bottom: 30px;">
-<div class="col-lg-4">
-            <img class="rounded-circle" src="../Docs/trainer1.jpg" alt="Trainer1" width="140" height="140">
-            <p>Tomas</p>
-            <p>Specialty: Resistance training.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
- </div>
-  <div class="col-lg-4">
-            <img class="rounded-circle" src="../Docs/trainer2.png" alt="trainer2" width="140" height="140">
-            <p>Ashley</p>
-            <p>Specialty: Fat reduction and muscle gain.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
- </div>
-  <div class="col-lg-4">
-            <img class="rounded-circle" src="../Docs/trainer3.jpg" alt="Traine3" width="140" height="140">
-            <p>Callum</p>
-            <p>Specialty: Hypertrophy and strength training.</p>
-            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
-          </div>
-</div>
-</div>
-</div>
-</section>
-
-//<?php
-//include("../includes/footer.php");
-//?>
+    
+<?php
+include("../includes/footer.php");
+?>
 </body>
 </html>
