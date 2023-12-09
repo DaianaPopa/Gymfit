8d1e6487078e717840736413e604c5eb3d19cc86
DaianaPopa <116299749+DaianaPopa@users.noreply.github.com>
2023-12-09 19:58:25 +0000

    Update ourTrainers.php
    ~ourTrainer~ page is done yay

--- Views/ourTrainers.php
+++ Views/ourTrainers.php
@@ -1,38 +1,86 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main Page</title>
+    <title>GymFit :: Our Trainers</title>
     <link rel="icon" type="image/x-icon" href="../favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-fmGlAWLI3Go9un1WX2Ut5vleFxSqyF6+RsTID0Cwj2P4M//pBuPvZP9tmIhOZsjcRdbjN4JQTmzXppGQV9wqGg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link
       rel="stylesheet"
-      href="css/home.css"
-
-    />
-    <script src="js/menu.js" ></script>
+      href="../CSS/home.css"
+      href="../CSS/main.css"
+  />
+  
 </head>
 <body>
-    <div class="mobile-container">
-   
+<?php
+include("../includes/header.php");
+?>
+<body background = "../Docs/cover2.jpg" 
+style= "background-size: cover">
+
+  
+  <section>
+<div class="projects grid">
+<div class="container">
+    <h1 class="title">Our trainers</h1>
+    <h5><p>Personal trainers work with you one-on-one to help design an engaging exercise regimen, help you with form, ensure you are working all your muscle groups safely, and more.</p></h5>
     
-      <div class="topnav">
-        <img
-        src="images/GymLogo.jpg"
-        width="50"
-        height="50"
-      />
-        <!-- Navigation links (hidden by default) -->
-        <div id="myLinks">
-          <a href="#about">About Us</a>
-          <a href="#macro">Macro Tracker</a>
-          <a href="#trainers">Our Trainers</a>
-          <a href="#workout">Work Out Planner</a>
-        </div>
-        <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
-        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-          <i class="fa fa-bars"></i>
-        </a>
-      </div>
-  </div>
+
+        
+
+<div class="row" style=" margin-bottom: 30px;">
+<div class="col-lg-4">
+            <img class="rounded-circle" src="../Docs/trainer1.jpg" alt="Trainer1" width="140" height="140">
+            <p>Tomas</p>
+            <p>Specialty: Resistance training.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+ </div>
+  <div class="col-lg-4">
+            <img class="rounded-circle" src="../Docs/trainer2.png" alt="trainer2" width="140" height="140">
+            <p>Ashley</p>
+            <p>Specialty: Fat reduction and muscle gain.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+ </div>
+  <div class="col-lg-4">
+            <img class="rounded-circle" src="../Docs/trainer3.jpg" alt="Traine3" width="140" height="140">
+            <p>Callum</p>
+            <p>Specialty: Hypertrophy and strength training.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+</div>
+</div>
+</div>
+</section>
+
+
+<section>
+<div class="projects grid">
+<div class="container">
+<div class="row" style=" margin-bottom: 30px;">
+<div class="col-lg-4">
+            <img class="rounded-circle" src="../Docs/trainer4.jpg" alt="Trainer1" width="140" height="140">
+            <p>Jack</p>
+            <p>Specialty: bodybuilding and condition.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+ </div>
+  <div class="col-lg-4">
+            <img class="rounded-circle" src="../Docs/trainer6.jpg" alt="trainer2" width="140" height="140">
+            <p>Claire</p>
+            <p>Specialty: weight loss and strength.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+ </div>
+  <div class="col-lg-4">
+            <img class="rounded-circle" src="../Docs/trainer5.jpg" alt="Traine3" width="140" height="140">
+            <p>Pia</p>
+            <p>Specialty: youth fitness and group fitness.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+          </div>
+</div>
+</div>
+</div>
+</section>
+
+</div>
+
 </body>
 </html>
