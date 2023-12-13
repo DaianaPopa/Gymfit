e6e6e2627e5c53e350576ce3ae6f7ba648721c60
PatJNewellSHU <c1013103@hallam.shu.ac.uk>
2023-12-13 01:25:38 -0800

    workout

--- Models/workoutModel.php
+++ Models/workoutModel.php
@@ -3,7 +3,7 @@ class Workout{
     public static function Add_Workout($reps,$sets,$day,){
         return;
     }
-    public static function Remove_Workout($ID){
+    public static function Remove_Workout($workout_ID,$member_ID){
         return;
     }
     public static function Alter_Workout($ID){
