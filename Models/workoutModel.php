5c529d33c8c2ab45c832a54ac01a13f9e8911c51
PatJNewellSHU <c1013103@hallam.shu.ac.uk>
2023-12-20 14:09:20 -0800

    workout model filled

--- Models/workoutModel.php
+++ Models/workoutModel.php
@@ -1,13 +1,26 @@
 <?php
 class Workout{
-    public static function Add_Workout($reps,$sets,$day,){
-        return;
+    public static function Add_Workout($reps,$sets,$day,$ID,$name){
+        $conn = Config :: Db_Connect();
+        $sql = "INSERT INTO workout(Member_ID,sets,reps,DOTW,workout)values($ID,$sets,$reps,$day)";
+        $conn->query($sql);
     }
     public static function Remove_Workout($workout_ID,$member_ID){
-        return;
+        $conn = Congfig :: Db_Connect();
+        $sql = "DELETE FROM workout where ID = '$workout_ID' and Member_ID = '$member_ID'";
+        $conn->query($sql);
     }
-    public static function Alter_Workout($ID){
-        return;
+    public static function Alter_Workout($ID,$reps,$sets,$member_ID,$name,$day){
+        $conn = Config :: Db_Connect();
+        $sql = "UPDATE workout SET reps = $reps, sets = $sets,DOTW = $day, workout = $name WHERE ID = $ID and Member_ID = $member_ID";
+        $conn->query($sql);
+    }
+
+    public static function Get_Workouts_By_Day($ID,$day){
+        $conn = Config :: Db_Connect();
+        $sql = "SELECT ID,sets,reps,workout WHERE Member_ID = $ID and DOTW = '$day'";
+        $result = $conn->query($sql);
+        return $result->fetch_assoc();
     }
 }
 ?>
