07369ad0527217fd681246aca28d383339775500
GitHoint <ozzyjosh03@gmail.com>
2023-12-22 15:17:36 +0000

    testywesty is no more

--- Testwesty.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("Models/ConfigModel.php");
-include("Models/MemberModel.php");
-echo "Welcome to testy westy";
-
-$pass = Gym_Member :: Get_Password_By_Email('bombadil@gmail.com');
-echo $pass;
-?>
